3863d8e9a5ee65b8c56d696e3f429b0aa535839d
agusariyantopage <agus.arianto21@gmail.com>
2022-08-06 21:41:21 +0800

    [0003] Ganti Versi Aplikasi 1.1

--- index.php
+++ index.php
@@ -92,7 +92,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">App Goldyan Noor 1.0</span>
+      <span class="brand-text font-weight-light">App Goldyan Noor 1.1</span>
     </a>
     
 
